f808ea9bb570d5d9dae7100c46497beda801d8d8
unknown <khaitruong1301@gmail.com>
2020-09-07 17:08:22 +0700

    'video_49_reactHook_baiTapBauCua_hieuUngXoayXucXac'

--- src/BaiTapTongHop/BaiTapGameBauCua/XucXac.js
+++ src/BaiTapTongHop/BaiTapGameBauCua/XucXac.js
@@ -1,22 +1,57 @@
 import React, { Fragment } from 'react'
+import {useSpring, animated} from 'react-spring'
+
 
 export default function XucXac(props) {
 
     let { xucXacItem } = props;
 
+    const [propsDice,set] = useSpring(()=> ( {
+        to: {
+            xyz:[1800,1800,1800]
+        },
+        from : {
+            xyz:[0,0,0]
+        },
+        config:{
+            duration:1000
+        },
+        reset:true
+    }))
+    
+    set({xyz:[1800,1800,1800]})
+
     return (
         <Fragment>
-            <div className="scene">
-                <div className="cube">
-                    <img className="ml-3 cube__face front" style={{ width: 50 }} src={xucXacItem.hinhAnh} />
-                    <img className="ml-3 cube__face back" style={{ width: 50 }} src="./img/BaiTapGameBauCua/bau.png" />
-                    <img className="ml-3 cube__face left" style={{ width: 50 }} src="./img/BaiTapGameBauCua/ga.png" />
-                    <img className="ml-3 cube__face right" style={{ width: 50 }} src="./img/BaiTapGameBauCua/ca.png" />
-                    <img className="ml-3 cube__face top" style={{ width: 50 }} src="./img/BaiTapGameBauCua/tom.png"/>
-                    <img className="ml-3 cube__face bottom" style={{ width: 50 }} src="./img/BaiTapGameBauCua/nai.png" />
-                    
+              <div className="scene ml-5">
+                    <animated.div  className="cube" style={{
+                        transform: propsDice.xyz.interpolate((x,y,z) => `translateZ(-25px) rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z}deg)`)
+                    }} >
+                        <div className="cube__face front">
+                            <img src={xucXacItem.hinhAnh} style={{ width: '100%' }} />
+                        </div>
+                        <div className="cube__face back">
+                            <img src='./img/BaiTapGameBauCua/cua.png' style={{ width: '100%' }} />
+                        </div>
+                        <div className="cube__face right">
+                            <img src='./img/BaiTapGameBauCua/tom.png' style={{ width: '100%' }} />
+
+                        </div>
+                        <div className="cube__face left">
+                            <img src='./img/BaiTapGameBauCua/ca.png' style={{ width: '100%' }} />
+
+                        </div>
+                        <div className="cube__face top">
+                            <img src='./img/BaiTapGameBauCua/ga.png' style={{ width: '100%' }} />
+
+                        </div>
+                        <div className="cube__face bottom">
+                            <img src='./img/BaiTapGameBauCua/nai.png' style={{ width: '100%' }} />
+
+                        </div>
+                    </animated.div>
+
                 </div>
-            </div>
         </Fragment>
     )
 }
